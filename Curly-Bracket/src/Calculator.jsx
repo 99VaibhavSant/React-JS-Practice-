9d0c315e48fe8f4d088f2cf61174860fed7981d9
99vaibhavsant <99vaibhavsant@gmail.com>
2025-11-08 17:42:59 +0530

    UnderStanding curly bracket

---
+++ Curly-Bracket/src/Calculator.jsx
@@ -0,0 +1,13 @@
+function Calculator() {
+  return (
+    <div>
+        {/* if we right in curly bracket we can do any javascript operation */}
+      <h1>Add to Number </h1>
+      <h2> 2 + 2 = {2 + 2}</h2>
+      <h1>Substract of 2 number</h1>
+      <h2>10 - 2 = {10 - 2}</h2>
+    </div>
+  );
+}
+
+export default Calculator;
